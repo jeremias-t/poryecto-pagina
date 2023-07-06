d1d52363f8e80072585c3620e2d2f79a3cd18468
Jeremias <torres.bjeremias@gmail.com>
2023-07-06 17:17:46 -0300

    6/7/2023

---
+++ src/components/cartas-comp.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-card width="100%" class="grey darken-4 mt-16">
+    <v-container class="">
+      <v-item-group v-model="selected" multiple>
+        <v-row>
+          <v-col v-for="(item, i) in items" :key="i" cols="12" md="3">
+            <v-item v-slot="{ active, toggle }">
+              <v-img
+                :src="`https://cdn.vuetifyjs.com/images/${item.src}`"
+                height="150"
+                class="text-right pa-2"
+                @click="toggle"
+              >
+                <v-btn icon dark>
+                  <v-icon>
+                    {{ active ? "mdi-heart" : "mdi-heart-outline" }}
+                  </v-icon>
+                </v-btn>
+              </v-img>
+            </v-item>
+          </v-col>
+        </v-row>
+      </v-item-group>
+    </v-container>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    items: [
+      {
+        src: "backgrounds/bg.jpg",
+      },
+      {
+        src: "backgrounds/md.jpg",
+      },
+      {
+        src: "backgrounds/bg-2.jpg",
+      },
+      {
+        src: "backgrounds/md2.jpg",
+      },
+    ],
+    selected: [],
+  }),
+};
+</script>
---
+++ src/components/fondo-comp.vue
@@ -0,0 +1,72 @@
+<template>
+  <v-app id="inspire">
+    <v-app-bar
+      app
+      color="grey darken-4
+"
+      flat
+    >
+      <v-container class="py-0 fill-height">
+        <v-btn v-for="link in links" :key="link" text class="white--text">
+          {{ link }}
+        </v-btn>
+
+        <v-spacer></v-spacer>
+
+        <v-responsive max-width="260">
+          <v-text-field
+            dense
+            class="grey darken-1"
+            flat
+            hide-details
+            rounded
+            solo-inverted
+          ></v-text-field>
+        </v-responsive>
+      </v-container>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" app width="300" class="grey darken-3">
+      <v-list-item>
+        <v-list-item-title class="headline mt-16 white--text pl-8"
+          >Lancer Work</v-list-item-title
+        >
+      </v-list-item>
+      <v-list class="pt-16 pl-8" shaped>
+        <v-list-item v-for="iten in itens" :key="iten" class="white--text" link>
+          <v-list-item-content>
+            <v-list-item-title> {{ iten }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main class="grey darken-4 main">
+      <filtro-comp />
+      <cartas-comp />
+    </v-main>
+  </v-app>
+</template>
+
+<style scoped>
+li {
+  text-decoration: none;
+}
+
+.main {
+  padding: 0;
+}
+</style>
+
+<script>
+import CartasComp from "./cartas-comp.vue";
+import filtroComp from "./filtro-comp.vue";
+export default {
+  components: { filtroComp, CartasComp },
+  data: () => ({
+    links: ["My Files", "Cloud", "Settings"],
+    itens: ["Dashboard", "Conversation", "My store", "Candidates"],
+  }),
+  drawer: null,
+};
+</script>
